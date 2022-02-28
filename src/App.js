ecd6ebf103ab6d5ba2b88d01f077b0941335bc12
Chris-cod <djokenvicky@gmail.com>
2022-02-28 23:53:03 +0100

    initialisation of the Project

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+ 
+} from "react-router-dom";
+
+
+
+import './App.css';
+import Header from './components/Header'
+import NotesListPage from './pages/NotesListPage';
+import NotePage from './pages/NotePage'
+
+function App() {
+  return (
+    <Router>
+      <div className="container dark">
+        <div className="app">
+          <Header />
+          <Routes>
+            <Route path='/' element={<NotesListPage />} />
+            <Route path='/note/:id' element={<NotePage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
---
+++ src/pages/NotePage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import notes from '../assets/data'
+import {useParams, Link} from "react-router-dom"
+import {ReactComponent as Arrowleft} from '../assets/arrow-left.svg'
+
+
+
+const NotePage = () => {
+    const params = useParams()
+    let noteId = params.id
+
+    let note = notes.find(note => note.id === Number(noteId))
+    return (
+        <div className="note">
+            <div className="note-header">
+                <h3>
+                    <Link to= "/" >
+                        <Arrowleft />
+                    </Link>
+                </h3>
+            </div>
+            <textarea value={note?.body}>
+
+            </textarea>
+        </div>
+    )
+}
+
+export default NotePage
