20c4937f1758782820a2010d3d2beb256c82a6d4
Chris-cod <djokenvicky@gmail.com>
2022-03-01 01:12:26 +0100

    add eson-server backen and make a get request on the server

--- src/pages/NotePage.js
+++ src/pages/NotePage.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import notes from '../assets/data'
+import React, {useEffect, useState} from 'react'
+//import notes from '../assets/data'
 import {useParams, Link} from "react-router-dom"
 import {ReactComponent as Arrowleft} from '../assets/arrow-left.svg'
 
@@ -9,7 +9,19 @@ const NotePage = () => {
     const params = useParams()
     let noteId = params.id
 
-    let note = notes.find(note => note.id === Number(noteId))
+    //let note = notes.find(note => note.id === Number(noteId))
+    let [note, setNote] = useState(null)
+
+    useEffect(() => {
+        getNote()
+    }, [noteId])
+
+    let getNote = async () => {
+        let response = await fetch (`http://localhost:8000/notes/${noteId}`)
+        let data = await response.json()
+        setNote(data)
+    }
+
     return (
         <div className="note">
             <div className="note-header">
