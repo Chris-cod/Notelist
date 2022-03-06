568583b224aeb99efb1406c79f1c62cb03a52070
Chris-cod <djokenvicky@gmail.com>
2022-03-07 00:54:20 +0100

    implement the create,update,delete and deploy the project in netlify

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,19 +1,5 @@
 let notes = [
-    {
-        "id": 1,
-        "body" : "Todays Agenda \n\n -wakeup and wash the tees\n -eat patatos salats\n- watch animes. ",
-        "updated" : "30-12-2021"
-    },
-    {
-        "id": 2,
-        "body" : "Tomorows Agenda \n\n -wakeup and wash the tees\n -travels to meet friend\n- back and cook a fish. ",
-        "updated" : "30-12-2021"
-    },
-    {
-        "id": 3,
-        "body" : "First days of the new year \n\n -wakeup and wash the tees\n -try to cook someting \n- calls family and Friend to wish happy new Year. ",
-        "updated" : "30-12-2021"
-    }
+
 ]
 
 export default notes;
